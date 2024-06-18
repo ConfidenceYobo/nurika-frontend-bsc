fc76182c5d7d1176a9477436c0850dc561c4b220
Emmanuel Alozie <emmanuelalozie@Emmanuels-MacBook-Pro.local>
2024-06-18 16:19:24 +0100

    chore: changed CTA 'BuyNow' to 'Mint Now'

--- src/components/container/mint/checkout.tsx
+++ src/components/container/mint/checkout.tsx
@@ -157,7 +157,7 @@ function Counter({ selectedNFT }: { selectedNFT: string }) {
           onClick={() => onBuyHandle()}
           className="w-full md:w-auto md:px-10"
         >
-          Buy Now
+          Mint Now
         </Button>
       ) : (
         <ConnectWallet>
